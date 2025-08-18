5a4ee1da6a4330499992f4c7eaba99a2c6ad2902
markfisher <mark@modulewise.com>
2025-08-18 11:00:04 -0400

    counter src more readable
    Signed-off-by: markfisher <mark@modulewise.com>

--- components/counter/src/lib.rs
+++ components/counter/src/lib.rs
@@ -10,13 +10,13 @@ struct Counter;
 
 impl exports::modulewise::demo::counter::Guest for Counter {
     fn count(thing: String) -> String {
-        let c = modulewise::demo::incrementor::increment(&thing).unwrap_or(0);
-        let t = if c == 1 { thing } else { format!("{thing}s") };
-        let s = wasi::config::store::get("suffix")
+        let count = modulewise::demo::incrementor::increment(&thing).unwrap_or(0);
+        let thing = if count == 1 { thing } else { format!("{thing}s") };
+        let suffix = wasi::config::store::get("suffix")
             .ok()
             .flatten()
             .unwrap_or("!".to_string());
-        format!("{c} {t} {s}")
+        format!("{count} {thing} {suffix}")
     }
 }
 
