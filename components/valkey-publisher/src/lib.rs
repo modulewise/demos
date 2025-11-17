95779537b106a963beee5742f46f72b3db7103bf
Mark Fisher <mark@modulewise.com>
2025-11-17 13:08:12 -0500

    add valkey-publisher component (#10)
    add valkey-publisher component
    
    Signed-off-by: markfisher <mark@modulewise.com>

---
+++ components/valkey-publisher/src/lib.rs
@@ -0,0 +1,40 @@
+#![no_main]
+
+use componentized::valkey::store::{self as valkey, HelloOpts};
+
+wit_bindgen::generate!({
+    path: "../wit",
+    world: "valkey-publisher-world",
+    generate_all
+});
+
+struct Publisher;
+
+impl exports::modulewise::demo::publisher::Guest for Publisher {
+    fn publish(message: String) -> Result<i64, String> {
+        let host = get_config_value("host", "127.0.0.1");
+        let port: u16 = get_config_value("port", "6379")
+            .parse()
+            .expect("failed to parse port");
+        let channel = get_config_value("channel", "modulewise");
+        let opts = HelloOpts {
+            proto_ver: Some("3".to_string()),
+            auth: None,
+            client_name: None,
+        };
+        let connection =
+            valkey::connect(&host, port, Some(&opts)).expect("failed to connect to valkey");
+        connection
+            .publish(&channel, &message)
+            .map_err(|e| e.to_string())
+    }
+}
+
+fn get_config_value(key: &str, default: &str) -> String {
+    wasi::config::store::get(&key)
+        .ok()
+        .flatten()
+        .unwrap_or(default.to_string())
+}
+
+export!(Publisher);
