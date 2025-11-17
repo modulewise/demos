415f6ec54b41ec52a3109fb84d572a15d1027428
markfisher <mark@modulewise.com>
2025-11-17 09:38:55 -0500

    use config for publisher
    Signed-off-by: markfisher <mark@modulewise.com>

--- components/publisher/src/lib.rs
+++ components/publisher/src/lib.rs
@@ -12,17 +12,27 @@ struct Publisher;
 
 impl exports::modulewise::demo::publisher::Guest for Publisher {
     fn publish(message: String) {
+        let host = get_config_value("host", "127.0.0.1");
+        let port: u16 = get_config_value("port", "6379").parse().expect("failed to parse port");
+        let channel = get_config_value("channel", "modulewise");
         let opts = HelloOpts {
             proto_ver: Some("3".to_string()),
             auth: None,
             client_name: None,
         };
         let connection =
-            valkey::connect("127.0.0.1", 6379, Some(&opts)).expect("failed to connect to valkey");
+            valkey::connect(&host, port, Some(&opts)).expect("failed to connect to valkey");
         connection
-            .publish("modulewise", &message)
+            .publish(&channel, &message)
             .expect("failed to publish message");
     }
+}
+
+fn get_config_value(key: &str, default: &str) -> String {
+    wasi::config::store::get(&key)
+        .ok()
+        .flatten()
+        .unwrap_or(default.to_string())
 }
 
 export!(Publisher);
